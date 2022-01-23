5e63365e1c596e7bbb795abdb3a5b48ead0c5a43
kuptsou.ai <kuptsou.ai@gmail.com>
2022-01-23 21:38:05 +0300

    SW-1 - Deleted log4j configuration setting.

--- src/main/java/org/sonatype/mavenbook/weather/Main.java
+++ src/main/java/org/sonatype/mavenbook/weather/Main.java
@@ -1,18 +1,11 @@
 package org.sonatype.mavenbook.weather;
 
-import org.apache.log4j.PropertyConfigurator;
-
 import java.io.InputStream;
 
 public class Main {
     private static final String DEFAULT_CITY = "London";
 
     public static void main(String[] args) throws Exception {
-// Configure Log4J
-        PropertyConfigurator
-                .configure(Main.class.getClassLoader()
-                        .getResource("log4j.properties"));
-
         String cityName = args.length != 0 ? args[0] : DEFAULT_CITY;
         InputStream dataIn = new OpenWeatherRetriever().retrieve(cityName);
         Weather weather = new OpenWeatherParser().parse(dataIn);
